d74f879318863b3c37770b04d6ae4391546f792d
steveimc <steveimc08@gmail.com>
2016-01-14 20:28:17 -0800

    FullLevelScene is the game scene

--- Assets/Managers/Game/GameManager.cs
+++ Assets/Managers/Game/GameManager.cs
@@ -150,7 +150,7 @@ namespace Ascent.Managers.Game
                 copyrightText.gameObject.SetActive(false);
             });
 
-            var loadTask = Application.LoadLevelAdditiveAsync("GameScene");
+            var loadTask = Application.LoadLevelAdditiveAsync("FullLevelScene");
 
             while (!loadTask.isDone)
                 yield return null;
